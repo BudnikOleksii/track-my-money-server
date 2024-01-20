83980ac4927ce936d1f0daece3738f9959e72386
Oleksii Budnik <oleksii.b.jsn@gmail.com>
2024-01-20 20:27:41 +0200

    test

--- src/modules/auth/dto/user-email.dto.ts
+++ src/modules/auth/dto/user-email.dto.ts
@@ -7,6 +7,6 @@ import { lowerCaseTransformer } from '../../../common/transformers';
 export class UserEmailDto {
   @ApiProperty({ example: 'user@gmail.com', description: 'Valid email' })
   @Transform(lowerCaseTransformer)
-  @IsEmail({}, { message: 'Email should be a valid email' })
+  @IsEmail({}, { message: 'Email should be a valid emai' })
   email: string;
 }
